e584b0cfccb3dd5e9f4f1b5d158b83da54aacfe2
Sandy Maguire <sandy@sandymaguire.me>
2021-01-07 13:25:01 -0800

    Initial commit

---
+++ lecture4-divisibility.lean
@@ -0,0 +1,87 @@
+import data.int.basic
+import data.nat.parity
+import data.nat.prime
+import tactic
+import tactic.linarith
+
+namespace lecture4
+
+open nat
+
+theorem div_14_div_7 : ∀ N, 14 ∣ N → 7 ∣ N :=
+begin
+  intros N h,
+  cases h with divisor h,
+  refine dvd.intro _ _,
+  let divisor' := 2 * divisor,
+  use divisor',
+  simp,
+  linarith,
+end
+
+theorem prime_and_prime_succ : ∀ p, prime p → prime (p + 1) → p = 2 :=
+begin
+  intros p hprime hprimesucc,
+  cases even_or_odd p,
+  {
+    cases h with n hn,
+    let hprime2 := hprime,
+    cases hprime,
+    specialize hprime_right n,
+    have n_div_p : n ∣ p,
+    {
+      exact dvd.intro_left 2 (eq.symm hn)
+    },
+
+    cases hprime_right n_div_p,
+    {
+      linarith,
+    },
+
+    have p_zero : p = 0,
+    {
+      rw h at hn,
+      linarith,
+    },
+
+    by_contradiction h',
+    apply not_prime_zero,
+    rw p_zero at hprime2,
+    exact hprime2,
+  },
+
+  let h2 := h,
+  cases h with n hn,
+  cases hprimesucc,
+
+  have n_div_succ_p : 2 ∣ succ p,
+  {
+    refine even_iff_two_dvd.mp _,
+    refine even_succ.mpr _,
+    exact odd_iff_not_even.mp h2,
+  },
+
+  specialize hprimesucc_right 2,
+  cases hprimesucc_right n_div_succ_p,
+  {
+    by_contradiction h',
+    linarith,
+  },
+
+  have n_zero : n = 0,
+  {
+    linarith,
+  },
+
+  rw n_zero at hn,
+  by_contradiction,
+  refine not_prime_one _,
+  have p_one : p = 1,
+  {
+    linarith,
+  },
+  rw ←p_one,
+  exact hprime,
+end
+
+end lecture4
