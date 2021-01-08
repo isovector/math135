a3fd8d5af7d83721cf80b527f68fafab4a05a21d
Sandy Maguire <sandy@sandymaguire.me>
2021-01-07 22:40:40 -0800

    Slightly better implementation for prime_and_prime_succ

--- lecture4-divisibility.lean
+++ lecture4-divisibility.lean
@@ -84,4 +84,37 @@ begin
   exact hprime,
 end
 
+
+lemma only_even_prime_is_2 : ∀ {n}, prime n -> even n -> n = 2 :=
+begin
+  intros n hp he,
+  cases hp,
+  cases he,
+  specialize hp_right 2,
+  cases hp_right (dvd.intro he_w (eq.symm he_h) : 2 ∣ n),
+  {
+    finish,
+  },
+  linarith,
+end
+
+theorem prime_and_prime_succ2
+      {p} (hprime : prime p) (hprimesucc : prime (p + 1)) : p = 2 :=
+begin
+  cases even_or_odd p,
+  {
+    exact only_even_prime_is_2 hprime h,
+  },
+  exfalso,
+  have p_succ_even : even (p + 1) := begin
+    refine even_succ.mpr _,
+    exact odd_iff_not_even.mp h,
+  end,
+  have p_succ_is_two : (p + 1) = 2 := only_even_prime_is_2 hprimesucc p_succ_even,
+  have p_is_one : p = 1 := by linarith,
+  refine not_prime_one _,
+  rw p_is_one at hprime,
+  exact hprime,
+end
+
 end lecture4
