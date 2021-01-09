e4c155bf4d1844468f9e2e206e893686f9260408
Sandy Maguire <sandy@sandymaguire.me>
2021-01-08 21:59:42 -0800

    Prove union eq inter of two sets iff they are equal

--- src/lecture6.lean
+++ src/lecture6.lean
@@ -1,17 +1,64 @@
-import data.nat.basic
-import data.nat.parity
 import tactic
 import data.set
+import data.set.basic
 
 namespace lecture6
 
-open nat
 open set
 
-example : set.subset {n | 4 ∣ n + 1} {k | 2 * k + 1} :=
+-- example :  {n | 4 ∣ n + 1} ⊆ {k | 2 * k + 1} := by sorry
+
+lemma eq_intersect_union_one_contains_other 
+    {α} 
+    {s t : set α} 
+    (union_eq_inter : s ∪ t = s ∩ t)
+    : s ⊆ t := 
 begin
-  
+  rw set.subset_def,
+  intros x x_in_s,
+  have s_in_union : s ⊆ s ∪ t := subset_union_left s t,
+  have intersect_in_t : s ∩ t ⊆ t := inter_subset_right s t,
+  refine mem_of_subset_of_mem intersect_in_t _,
+  rw ← union_eq_inter,
+  refine mem_of_subset_of_mem s_in_union _,
+  exact x_in_s,
 end
 
-end lecture6
+example : Π {α : Type} (s t : set α), s = t ↔ s ∪ t = s ∩ t :=
+begin
+  intros a s t,
+  split,
+  {
+    intro s_eq_t,
+    refine ext _,
+    intro x,
+    split,
+    {
+      intro x_in_union,
+      rw s_eq_t at *,
+      rw union_self at x_in_union,
+      rw inter_self,
+      exact x_in_union,
+    },
+    intro x_in_inter,
+    rw s_eq_t at *,
+    rw union_self,
+    rw inter_self at x_in_inter,
+    exact x_in_inter,
+  },
+  intro union_eq_intersect,
+  
+  have s_in_t : s ⊆ t := eq_intersect_union_one_contains_other union_eq_intersect,
+  have t_in_s : t ⊆ s := 
+  begin
+    refine eq_intersect_union_one_contains_other _,
+    rw union_comm,
+    rw inter_comm,
+    exact union_eq_intersect,
+  end,
 
+  exact le_antisymm s_in_t t_in_s,
+end
+
+
+end lecture6
