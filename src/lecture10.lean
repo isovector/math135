c201e8a989b95c8a129294c2a706d16e5ee063d6
Sandy Maguire <sandy@sandymaguire.me>
2021-01-11 12:38:47 -0800

    sqrt2 is irrational!

--- src/lecture10.lean
+++ src/lecture10.lean
@@ -4,60 +4,71 @@ import data.nat.prime
 import data.real.basic
 import data.nat.gcd
 import data.real.sqrt
+import data.nat.parity
+import tactic.slim_check
 
 namespace lecture10
 
 open nat
 
-def irrational (r : ℝ) := ∀ (n d : ℕ), coprime n d -> ¬ coe n / coe d = r
+def irrational (r : ℝ) := ∀ (n d : ℕ), (d ≠ 0) -> coprime n d -> ¬ coe n / coe d = r
 
 lemma not_coprime_if_even {a b : ℕ} (even_a : even a) (even_b : even b) : ¬ (coprime a b) := 
 begin
-  cases even_a with i hi,
-  cases even_b with j hj,
-  have two_div_a : 2 ∣ a := by sorry,
-  have two_div_b : 2 ∣ b := by sorry,
   intro h_coprime,
-  simp [coprime] at h_coprime,
-  have h : a.gcd b ≥ 2 := begin
-    rw hi,
-    rw hj,
-    simp,
-    sorry,
+  rw [coprime] at h_coprime,
+  have two_div_coprime : 2 ∣ gcd a b := begin
+    exact dvd_gcd even_a even_b,
   end,
-  rw h_coprime at h,
-  linarith,
+  rw h_coprime at two_div_coprime,
+  finish,
 end
 
 lemma even_iff_even_square {a : ℕ} : even a ↔ even (a ^ 2) :=
 begin
-  sorry
+  fconstructor,
+  {
+    intro h,
+    refine even_iff_two_dvd.mpr _,
+    cases h with k hk,
+    rw hk,
+    rw pow_two,
+    refine dvd_mul_of_dvd_left _ (2 * k),
+    exact dvd.intro k rfl,
+  },
+  {
+    intro h,
+    refine even_iff_two_dvd.mpr _,
+    rw pow_two at h,
+    rw even_mul at h,
+    finish,
+  },
 end
 
 lemma irrational_sqrt_2 : irrational (real.sqrt 2) := 
 begin
   rw irrational,
-  intros n d h,
+  intros n d d_neq_zero h,
   by_contra,
 
   have h' : n^2 = 2 * d^2 := 
   begin
-    have x : coe n = real.sqrt 2 * coe d := by sorry,
+    have x : coe n = real.sqrt 2 * coe d := begin
+      refine (div_eq_iff _).mp h,
+      exact cast_ne_zero.mpr d_neq_zero,
+    end,
     have x' : (coe n)^2 = (real.sqrt 2)^2 * (coe d)^2 := 
       tactic.ring_exp.pow_pp_pf_prod x rfl rfl,
     rw real.sqr_sqrt at x',
-    {
-      
-    },
-
+    assumption_mod_cast,
+    norm_num,
   end,
-    
-
   
   have even_pow_n_2 : even (n^2) := begin
     refine even_iff_two_dvd.mpr _,
-    sorry,
+    finish,
   end,
+
   have even_n : even n := even_iff_even_square.mpr even_pow_n_2,
   have even_n' : even n := even_n,
 
@@ -73,12 +84,12 @@ begin
   have even_pow_d_2 : even (d^2) := 
   begin
      refine even_iff_two_dvd.mpr _,
-     sorry,
+     finish,
   end,
   have even_d : even d := even_iff_even_square.mpr even_pow_d_2,
 
   have not_coprime : ¬ coprime n d := not_coprime_if_even even_n' even_d,
-  sorry,
+  contradiction,
 end
 
 end lecture10
