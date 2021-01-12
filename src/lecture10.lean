251b161e398760679d583d010bf1970efdd09c40
Sandy Maguire <sandy@sandymaguire.me>
2021-01-12 06:14:29 -0800

    Reworked irrationality of sqrt2 proof

--- src/lecture10.lean
+++ src/lecture10.lean
@@ -17,7 +17,8 @@ lemma not_coprime_if_even {a b : ℕ} (even_a : even a) (even_b : even b) : ¬ (
 begin
   intro h_coprime,
   rw [coprime] at h_coprime,
-  have two_div_coprime : 2 ∣ gcd a b := dvd_gcd even_a even_b,
+  have two_div_coprime : 2 ∣ gcd a b, 
+  { exact dvd_gcd even_a even_b },
   rw h_coprime at two_div_coprime,
   finish,
 end
@@ -46,47 +47,47 @@ end
 lemma irrational_sqrt_2 : irrational (real.sqrt 2) := 
 begin
   rw irrational,
-  intros n d d_neq_zero h,
+  intros n d d_neq_zero hcoprime,
   by_contra,
-
-  have h' : n^2 = 2 * d^2 := 
-  begin
-    have x' : (coe n)^2 = (real.sqrt 2)^2 * (coe d)^2 := 
-    begin
-      refine tactic.ring_exp.pow_pp_pf_prod _ rfl rfl,
-      refine (div_eq_iff _).mp h,
-      exact cast_ne_zero.mpr d_neq_zero,
-    end,
+  have not_coprime : ¬ coprime n d,
+  
+  replace h : n^2 = 2 * d^2,
+  {
+    have x' : (coe n)^2 = (real.sqrt 2)^2 * (coe d)^2,
+    refine tactic.ring_exp.pow_pp_pf_prod _ rfl rfl,
+    refine (div_eq_iff _).mp h,
+    exact cast_ne_zero.mpr d_neq_zero,
     rw real.sqr_sqrt at x',
     assumption_mod_cast,
     norm_num,
-  end,
+  },
 
-  have even_pow_n_2 : even (n^2) := begin
+  have even_n : even n,
+  {
+    refine even_iff_even_square.mpr _,
     refine even_iff_two_dvd.mpr _,
-    finish,
-  end,
+    exact dvd.intro (d ^ 2) (eq.symm h),
+  },
 
-  have even_n  : even n := even_iff_even_square.mpr even_pow_n_2,
-  have even_n' : even n := even_n,
+  refine not_coprime_if_even even_n _,
+  refine even_iff_even_square.mpr _,
 
-  cases even_n with k hk,
-  have yo :=
+  have even_pow_d_2 : even (d^2),
+  { 
+    refine even_iff_two_dvd.mpr _,
+    cases even_n with k hk,
+    replace h : 2 * d^2 = 2 * (2 * k^2),
     calc 
-      2 * d^2 = n^2            : h'.symm
+      2 * d^2 = n^2            : h.symm
           ... = (2 * k)^2      : by rw hk
           ... = 2^2 * k^2      : by linarith
           ... = 4 * k^2        : by linarith
           ... = 2 * (2 * k^2)  : by linarith,
-  replace h := (mul_right_inj' (by norm_num)).1 yo,
-  have even_pow_d_2 : even (d^2) := 
-  begin
-     refine even_iff_two_dvd.mpr _,
-     finish,
-  end,
-  have even_d : even d := even_iff_even_square.mpr even_pow_d_2,
+    replace h := (mul_right_inj' (by norm_num)).1 h,
+    exact dvd.intro (k ^ 2) (eq.symm h),
+  },
 
-  have not_coprime : ¬ coprime n d := not_coprime_if_even even_n' even_d,
+  assumption,
   contradiction,
 end
 
